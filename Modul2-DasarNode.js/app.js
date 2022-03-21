f89cd1ded2242cacf0225eaeab15e058eae972df
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-21 14:41:04 +0700

    Added syntax on app.js

--- Modul2-DasarNode.js/app.js
+++ Modul2-DasarNode.js/app.js
@@ -2,3 +2,10 @@
 const server = new Server({
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com',
 });
+
+/* Jalankan pada Windows CMD
+> SET NODE_ENV=production && node app.js 
+*/
+
+/* // perintah ini tidak akan berjalan
+node -e 'process.env.foo = "bar"' && echo $foo */
