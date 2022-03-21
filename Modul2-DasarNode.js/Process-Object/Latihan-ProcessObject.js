9e3dfd9d084b71612c993ce602c5cac8e8eabfb0
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-21 14:54:54 +0700

    Added Latihan-ProcessObject

---
+++ Modul2-DasarNode.js/Process-Object/Latihan-ProcessObject.js
@@ -0,0 +1,20 @@
+const initialMemoryUSage = // TODO 1
+const yourName =  // TODO 2
+const environment = // TODO 3
+
+for(let i = 0; i <= 10000; i++) {
+    //Proses looping ini akan membuat penggunaan memori naik
+}
+
+const currentMemoryUsage= // TODO 4
+
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`)
+console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
+
+/* Selesaikan kode yang ditandai TODO dengan ketentuan berikut:
+TODO 1 : Isi dengan nilai heapUsed dari instance process.memoryUsage.
+TODO 2 : Isi dengan nilai index ke-2 dari process.argv.
+TODO 3 : Isi dengan nilai NODE_ENV dari process.env.
+TODO 4 : Isi dengan nilai heapUsed dari instance process.memoryUsage. 
+*/
