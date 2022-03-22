ebf6721aaa680a72af214a00868a7f1cce0826c9
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-22 14:09:57 +0700

    Added FileSystem

---
+++ Modul2-DasarNode.js/Filesystem/FileSystem.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const fileReadCallback = (error, data) => {
+    if(error) {
+        console.log('Gagal membaca berkas');
+        return;
+    }
+    console.log(data);
+};
+
+fs.readFile('todo.txt', 'UTF-8', fileReadCallback);
+
+/* //! Alternatif method versi synchronous fs.readFileSync() 
+* > const data = fs.readFileSync('todo.txt', 'UTF-8);
+* > console.log(data);
+*/
