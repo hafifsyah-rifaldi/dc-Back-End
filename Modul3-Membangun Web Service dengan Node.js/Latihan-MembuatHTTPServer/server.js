71428157d5c193a39aed1a85354b2200f15fe616
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-23 14:47:45 +0700

    Added ResponseStatus on server.js

--- Modul3-Membangun Web Service dengan Node.js/Latihan-MembuatHTTPServer/server.js
+++ Modul3-Membangun Web Service dengan Node.js/Latihan-MembuatHTTPServer/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
+    
 
     // * Menambahkan Handling Request Method dan properti url
     const { method, url } = request;
@@ -10,14 +10,17 @@ const requestListener = (request, response) => {
     if(url === '/') {
         // ! TODO 2: logika respons bila url bernilai '/'
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Ini adalah homepage</h1>');
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
 
     } else if(url === '/about') {
         // ! TODO 3: logika respons bila url bernilai '/about'
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Halo! Ini adalah halaman about ');
         } else if(method === 'POST') {
             // ! Menambahkan logika stream blok POST
@@ -30,14 +33,17 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
+                response.statusCode = 200;
                 response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
                 });
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
         }
 
     } else {
         // ! TODO 1: logika respons bila url bukan '/' atau '/about'
+        response.statusCode = 404;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 
