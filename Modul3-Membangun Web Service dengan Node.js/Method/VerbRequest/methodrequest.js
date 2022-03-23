c773fa30b8fbeacda42d564b18abcaee5684e644
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-23 13:30:11 +0700

    Added methodRequest.js

---
+++ Modul3-Membangun Web Service dengan Node.js/Method/VerbRequest/methodrequest.js
@@ -0,0 +1,20 @@
+const requestListener = (request, response) => {
+    const method = request.method;
+};
+
+/*  Atau cara lebih clean
+* menggunakan object destructuring */
+
+const requestListener = (request, response) => {
+    const { method } = request;
+
+    if(method === 'GET') {
+        //response ketika GET
+    }
+
+    if(method === 'POST') {
+        //response ketika POST
+    }
+
+    // Anda bisa mengevaluasi tipe method lainnya
+};
