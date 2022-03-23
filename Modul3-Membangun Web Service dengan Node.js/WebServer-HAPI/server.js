85f0cb7073ab3391affc832037ada730517349fd
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-23 16:48:43 +0700

    Added WebServer-HAPI

--- Modul3-Membangun Web Service dengan Node.js/WebServer-HAPI/server.js
+++ Modul3-Membangun Web Service dengan Node.js/WebServer-HAPI/server.js
@@ -3,12 +3,15 @@
 */
 
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
         host: 'localhost',
     });
+
+    server.route(routes);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
@@ -25,4 +28,17 @@ init();
 ! Proses menjalankan server (server.start()) dilakukan secara asynchronous. Karena itu, kita perlu menjalankannya di dalam fungsi async dan memanggil server.start() menggunakan await.
 
 ! Setelah server berhasil berjalan, Anda bisa melihat alamat lengkap dan port di mana server dijalankan melalui properti server.info.uri.
+*/
+
+/* 
+* lakukan perintah !
+curl -X GET http://localhost:5000
+ output: Homepage
+curl -X GET http://localhost:5000/about
+ output: About page
+curl -X GET http://localhost:5000/test
+ output: Halaman tidak ditemukan
+curl -X POST http://localhost:5000
+ output: Halaman tidak dapat diakses dengan method tersebut
+
 */
