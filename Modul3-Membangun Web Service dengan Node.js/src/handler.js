551785783817c3e6c96265c3c05a3ac3a64f1497
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-28 14:00:42 +0700

    Added server,routes,notes,handler js for Catatan App

---
+++ Modul3-Membangun Web Service dengan Node.js/src/handler.js
@@ -0,0 +1,44 @@
+const { nano } = require('nanoid'); // import nanoid dari package module library nanoid
+const notes = require('./notes'); // import array dari notes.js
+
+
+const addNoteHandler = (request, h) => {
+    const { title, tags, body } = request.payload;
+
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newNote = {
+        title, tags, body, id, createdAt, updatedAt,
+    };
+
+    notes.push(newNote);
+
+    const isSuccess = notes.filter((note) => note.id === id).length > 0; // Memanfaatkan method filter() berdasar id catatan
+
+    // isSuccess untuk menentukan respons yang diberikan server
+    if (isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan berhasil ditambahkan',
+            data: {
+                noteId: id
+            },
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Catatan gagal ditambahkan',
+    });
+    response.code(500);
+    return response;
+
+
+};
+
+
+module.exports = { addNoteHandler };
